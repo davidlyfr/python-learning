0e062247314fb7d5cdbff31e0a7256a3dac19571
davidlyfr <davelynch45@gmail.com>
2017-08-26 17:53:21 +0200

    Learing to Speak Object Oriented Some probs

--- ex40.py
+++ ex40.py
@@ -14,3 +14,7 @@ bulls_on_parade = Song(["They rally around the family", "With pockets full of __
 happy_bday.sing_me_a_song()
 
 bulls_on_parade.sing_me_a_song()
+
+mywords = Song(["sing me a song", "I am the piano man", "la la la"])
+
+mywords.sing_me_a_song() 
