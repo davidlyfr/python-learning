239dc8396bc1d16ab3e7853b0de8d219c8c58f39
davidlyfr <davelynch45@gmail.com>
2017-08-29 22:27:38 +0200

    Printing Strings

---
+++ ex06.py
@@ -0,0 +1,19 @@
+x = "There are %d types of people." % 10
+binary = "binary"
+do_not = "dont't"
+y = "Those who konw %s and those who %s" % (binary, do_not)
+
+print x
+print y
+
+print "I said: %r" % x
+print "I also said: '%s'" % y
+
+funny = False
+joke_eval = "Isn't that joke so funny? %r"
+
+print joke_eval % funny
+w = "this is the left side...."
+e = "this is the right side"
+
+print w + e
