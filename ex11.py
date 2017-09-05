d2bf71b92f47f45032ee7cb5d7ebec92f9746e8c
davidlyfr <davelynch45@gmail.com>
2017-09-05 19:52:12 +0200

    time calls

--- ex11.py
+++ ex11.py
@@ -1,4 +1,9 @@
 print "what is your age",
 age = raw_input()
-
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
 print "you are %s years old" % (age)
+print "\n your age is %r, height %r and weight %s " % (age, height, weight)
+
