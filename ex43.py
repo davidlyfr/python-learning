7aad91fd357d34f906605b39eddc41a131714298
davidlyfr <davelynch45@gmail.com>
2017-11-13 18:10:31 +0000

    basic structure

--- ex43.py
+++ ex43.py
@@ -4,5 +4,54 @@ from random import randint
 class Scene(object):
 
     def enter(self):
-    print "This scene is not yet cnfigured. Subclass it and implement enter()."
+        print "This scene is not yet cnfigured. Subclass it and implement enter()."
         exit(1)
+
+class Engine(object):
+
+    def __init__(self, scene_map):
+        pass
+    
+    def play(self):
+        pass
+
+class Death(Scene):
+
+    def enter(self):
+    
+        pass
+
+class CentralCorridor(Scene):
+  
+    def enter(self):
+        pass
+
+class LazerWeaponArmory(Scene):
+
+    def enter(self):
+        pass
+
+class TheBridge(Scene):
+
+    def enter(self):
+        pass
+
+class EscapePod(Scene):
+
+    def enter(self):
+        pass
+
+class Map(object):
+
+    def __init__(self, start_scene):
+        pass
+
+    def next_scene(self, scene_name):
+        pass
+
+    def opening_scene(self):
+        pass
+
+a_map = Map('central_corridor')
+a_game = Engine(a_map)
+a_game.play()
