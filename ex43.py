fa9c28c2426b37f548d86f7218c3334ef42d4eda
davidlyfr <davelynch45@gmail.com>
2017-11-13 16:13:24 +0000

    building

---
+++ ex43.py
@@ -0,0 +1,8 @@
+from sys import exit
+from random import randint
+
+class Scene(object):
+
+    def enter(self):
+    print "This scene is not yet cnfigured. Subclass it and implement enter()."
+        exit(1)
