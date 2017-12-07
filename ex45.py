7014a387048d6450c2d2dec3de7176461c0fa83e
davidlyfr <davelynch45@gmail.com>
2017-12-07 18:41:38 +0000

    story for program

---
+++ ex45.py
@@ -0,0 +1,23 @@
+# exercise 45 - write your own story and convert to a game
+'''
+you find your self on a terminal, logged to a linux system that you are admin off - objects are:
+carry out recon on your network
+find a vulnerable service
+brute force your entry
+-- dont get too noisy
+-- cover your tracks when possible
+
+Login-own-server
+
+what to do - nmap scan - hail mary - nessus
+
+
+hacker discovered - nmap results
+
+find services - nessus - nmap - web request
+
+brute force choice - hydra - bree - zenmap
+
+access denied - accessed 
+
+'''
