bc0a7e618a2519514bac8a4401edae801a51e5c7
davidlyfr <davelynch45@gmail.com>
2017-12-09 17:32:31 +0000

    building classes for new story

--- ex45.py
+++ ex45.py
@@ -21,3 +21,56 @@ brute force choice - hydra - bree - zenmap
 access denied - accessed 
 
 '''
+
+class Terminal(object):
+ 
+    def enter(self):
+        pass
+
+class Engine(object):
+
+    def __init__(self,term_map):
+        pass
+
+    def play(self):
+        pass
+
+class Detected(Terminal):
+
+    def enter(self):
+        pass
+
+class Main_Terminal(Terminal):
+
+    def enter(self):
+        pass
+
+class FindingHosts(Terminal):
+
+    def enter(self):
+        pass
+
+class FindingServices(Terminal):
+
+    def enter(self):
+        pass
+
+class BruteForce(Terminal):
+
+    def enter(self):
+        pass
+
+class Map(object):
+
+    def __init__(self, start_term):
+        pass
+
+    def next_term(self, scene_name):
+        pass
+
+    def opening_term(self):
+        pass
+
+a_map = Map('main_terminal')
+a_game = Engine(a_map)
+a_game.play()
