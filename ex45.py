20c34bd5c66a0edf80370db1dec49a0eec02bf8e
davidlyfr <davelynch45@gmail.com>
2017-12-17 22:44:47 +0000

    Working

--- ex45.py
+++ ex45.py
@@ -58,33 +58,156 @@ class Detected(Terminal):
 class Main_Terminal(Terminal):
 
     def enter(self):
-        pass
+        print "You are logged in as Root on your own terminal."
+        print "The first step in any journey, is to go in the "
+        print "right direction."
+        print " "
+        print "Recon - find your target"
+        print "you now wear a Black-Hat - hacking a remote pc"
+        print "is your objective - lets find it! how?"
+        print "what tool do you use to find the target?"
+        print "nmap - Bing - Facebook friend finder"
+
+        action = raw_input("> ")
+  
+        if action == "nmap":
+            print "Good decision"
+            print " "
+#            print "Nmap scan report for 192.168.1.70"
+#            print "Host is up (0.0012s latency)."
+#            print "Not shown: 995 closed ports"
+#            print "PORT     STATE SERVICE"
+#            print "22/tcp   open  ssh"
+#            print "139/tcp  open  netbios-ssn"
+#            print "445/tcp  open  microsoft-ds"
+#            print "5050/tcp open  mmcc"
+#            print "5666/tcp open  nrpe"
+#            print "MAC Address: B8:27:EB:B3:3E:CB (Raspberry Pi Foundation)"
+#            print " "
+#            print "Nmap done: 1 IP address (1 host up) scanned in 5.95 seconds"
+            return 'finding_hosts'
+        elif action == "hail mary":
+            print "detected"
+            return 'detected_death'
+        elif action == "nessus":
+            print "detected"
+            return 'detected_death'
+        
 
 class FindingHosts(Terminal):
 
     def enter(self):
-        pass
+        print "Lets Ping scan the network,"
+        print "to find out the IP of neighboring hosts"
+        print "What is the correct parameter in the following line"
+        print "nmap -XX 192.168.1.0/24"
+        print " sn sN sV "
 
+        action = raw_input("> ")
+        
+        if  action == "sn":
+            print "Good decision"
+            print " "
+            print "$ nmap -sn 192.168.1.0/24"
+            print "Starting Nmap 6.40 ( http://nmap.org ) at 2017-12-17 21:28 UTC"
+            print "Nmap scan report for 192.168.1.12"
+            print "Host is up (0.00086s latency)."
+            print "Nmap scan report for 192.168.1.70 - Rasp VPN"
+            print "Host is up (0.00046s latency)."
+            print "Nmap scan report for 192.168.1.111"
+            print "Host is up (0.018s latency)."
+            print "Nmap scan report for router.asus.com (192.168.1.254)"
+            print "Host is up (0.0085s latency). "
+            print "Nmap done: 1 IP address (1 host up) scanned in 5.95 seconds"
+            print "Nmap done: 256 IP addresses (3 hosts up) scanned in 7.73 seconds"
+            return 'brute_force'
+        elif action == "sN":
+            print "detected"
+            return 'detected_death'
+        elif action == "sV":
+            print "detected"
+            return 'detected_death'
+        else:
+            print "Does not ring a bell!"
+            return 'finding_hosts'
 class FindingServices(Terminal):
 
     def enter(self):
-        pass
+        print "What tool is best for finding services? "
+        print "nmap - google - sans"
+        
+        action = raw_input("> ")
+ 
+        if action == "nmap":
+            print "Starting Nmap 6.40 ( http://nmap.org ) at 2017-12-17 19:40 UTC"
+            print "Nmap scan report for 192.168.1.70"
+            print "Host is up (0.0012s latency)."
+            print "Not shown: 995 closed ports"
+            print "PORT     STATE SERVICE"
+            print "22/tcp   open  ssh"
+            print "139/tcp  open  netbios-ssn"
+            print "445/tcp  open  microsoft-ds"
+            print "5050/tcp open  mmcc"
+            print "5666/tcp open  nrpe"
+            print "MAC Address: B8:27:EB:B3:3E:CB (Raspberry Pi Foundation)"
+            print " "
+            print "Nmap done: 1 IP address (1 host up) scanned in 5.95 seconds"
+            return 'brute_force'
+        elif action == "hail mary":
+            print "detected"
+            return 'detected_death'
+        elif action == "nessus":
+            print "detected"
+            return 'detected_death'
+        else:
+            print "Try again!"
+            return 'finding_services'
 
 class BruteForce(Terminal):
 
     def enter(self):
-        pass
+        print "We have chosen to attach the SSH protocol."
+        print "This type of attack requires parameters."
+        print "It finds authorised users, names and passwords."
+        print "Chose a tool - zenmap - hydra - passive DNS"
+
+        action = raw_input("> ")
+
+        if action == "hydra":
+            print "Hydra starting --- winner"
+            exit(1)
+        elif action == "zenmap":
+            print "This tool is a graphical version of nmap - that we just used."
+            print "Try again"
+            return 'brute_force'
+        elif action == "passive dns":
+            print "na - this tool is more for gathering information"
+            print "that may lead to a target of interest -"
+            print "like - Matching an SQL db to an IP"
+            return 'brute_force'
+        else:
+            print "try again- though you maybe right"
+            print "There are no straight lines in IT"
+            return 'brute_force'
 
 class Map(object):
 
+    scenes = {
+    'main_terminal': Main_Terminal(),
+    'finding_services': FindingServices(),
+    'detected_death': Detected(),
+    'finding_hosts': FindingHosts(),
+    'brute_force': BruteForce()
+    }
+
     def __init__(self, start_term):
-        pass
+        self.start_term = start_term
 
     def next_term(self, scene_name):
-        pass
+        return Map.scenes.get(scene_name)
 
     def opening_term(self):
-        pass
+        return self.next_term(self.start_term)
 
 a_map = Map('main_terminal')
 a_game = Engine(a_map)
