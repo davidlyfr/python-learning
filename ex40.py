5bf5a41195907fcc4221bc862a9038ccce2073ca
davidlyfr <davelynch45@gmail.com>
2017-12-17 18:53:52 +0000

    added comments

--- ex40.py
+++ ex40.py
@@ -8,11 +8,13 @@ class Song(object):
             print line
 
 happy_bday = Song(["Happy birthday to you", "I don't want to get sued", "So I'll stop right there"])
+# set 'happy_bday' to an instance of class Song, with list values, a, b & c
 
 bulls_on_parade = Song(["They rally around the family", "With pockets full of ___ lol"])
+# set 'bulls_on_parade' to an instance of class Song with values in a list structure
 
 happy_bday.sing_me_a_song()
-
+# get sing me a song function from the class set by the variable happy_bday - which has values in a list format
 bulls_on_parade.sing_me_a_song()
 
 mywords = Song(["sing me a song", "I am the piano man", "la la la"])
